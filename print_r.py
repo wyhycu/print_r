53b73c621e789ada28b7f4bedd77a70e585ed474
wyhycu <wyhycu@gmail.com>
2024-12-20 22:34:52 +0000

    Cleaned up comments

--- print_r.py
+++ print_r.py
@@ -32,35 +32,34 @@ def awesome_pretty_print_colors(obj, indent=0):
         print(' ' * indent + '{')
         for key, value in obj.items():
             print(' ' * (indent + 4) + Fore.CYAN + repr(key) + Style.RESET_ALL + ' => ', end='')
-            awesome_pretty_print_colors(value, indent + 4)  # Corrected function call
+            awesome_pretty_print_colors(value, indent + 4)  
         print(' ' * indent + '}')
     elif isinstance(obj, list):
         print(' ' * indent + '[')
         for index, value in enumerate(obj):
             print(' ' * (indent + 4) + Fore.YELLOW + f'[{index}]' + Style.RESET_ALL + ' => ', end='')
-            awesome_pretty_print_colors(value, indent + 4)  # Corrected function call
+            awesome_pretty_print_colors(value, indent + 4) 
         print(' ' * indent + ']')
     else:
         print(Fore.GREEN + repr(obj) + Style.RESET_ALL)
 
 def appc(obj):
-    awesome_pretty_print_colors(obj)  # Corrected function call
+    awesome_pretty_print_colors(obj)  
 
 # PHP style print_r with colors
-
 def php_style_print_r(obj, indent=0):
     spacing = '    '
     if isinstance(obj, dict):
         print(' ' * indent + '(')
         for key, value in obj.items():
             print(' ' * (indent + 4) + Fore.CYAN + repr(key) + Style.RESET_ALL + ' => ', end='')
-            php_style_print_r(value, indent + 4)  # Corrected function call
+            php_style_print_r(value, indent + 4) 
         print(' ' * indent + ')')
     elif isinstance(obj, list):
         print(' ' * indent + '(')
         for index, value in enumerate(obj):
             print(' ' * (indent + 4) + Fore.YELLOW + f'[{index}]' + Style.RESET_ALL + ' => ', end='')
-            php_style_print_r(value, indent + 4)  # Corrected function call
+            php_style_print_r(value, indent + 4)  
         print(' ' * indent + ')')
     else:
         print(Fore.GREEN + repr(obj) + Style.RESET_ALL)

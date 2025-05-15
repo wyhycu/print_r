f95a9cb0dd1e9120c911bc2daf49c660d37672fe
wyhycu <wyhycu@gmail.com>
2025-05-15 12:11:27 +0000

    Added python_style_print_r for experimentation

--- print_r.py
+++ print_r.py
@@ -66,3 +66,30 @@ def php_style_print_r(obj, indent=0):
 
 def ppr(obj):
     php_style_print_r(obj)
+
+# Python style print_r with colors with logic to handle python objects better
+def python_style_print_r(obj, indent=0):
+    spacing = '    '
+    if isinstance(obj, dict):
+        print(' ' * indent + '(')
+        for key, value in obj.items():
+            print(' ' * (indent + 4) + Fore.CYAN + repr(key) + Style.RESET_ALL + ' => ', end='')
+            python_style_print_r(value, indent + 4)
+        print(' ' * indent + ')')
+    elif isinstance(obj, list):
+        print(' ' * indent + '(')
+        for index, value in enumerate(obj):
+            print(' ' * (indent + 4) + Fore.YELLOW + f'[{index}]' + Style.RESET_ALL + ' => ', end='')
+            python_style_print_r(value, indent + 4)
+        print(' ' * indent + ')')
+    elif hasattr(obj, '__dict__'):
+        print(' ' * indent + obj.__class__.__name__ + ' {')
+        for key, value in vars(obj).items():
+            print(' ' * (indent + 4) + Fore.CYAN + repr(key) + Style.RESET_ALL + ' => ', end='')
+            python_style_print_r(value, indent + 4)
+        print(' ' * indent + '}')
+    else:
+        print(Fore.GREEN + repr(obj) + Style.RESET_ALL)
+
+def pypr(obj):
+    python_style_print_r(obj)
